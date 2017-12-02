261d981f106fa1dedffd395600d41a4d578c3e6d
FateWalker <opabravo@gmail.com>
2017-12-02 21:56:36 +0800

    Update gray.py

--- gray.py
+++ gray.py
@@ -50,7 +50,7 @@ class Gray:
         kiss = "**{0} gave {1} a kiss!**"
         kissself= "**{} Kissed himself :kiss: **"
         
-        choices = ['http://i.imgur.com/0D0Mijk.gif', 'https://i.imgur.com/4VePCc4.gif', 'http://i.imgur.com/3wv088f.gif', 'https://i.imgur.com/dG73Bmb.gif', '', 'https://i.imgur.com/5epo3Ls.gifv', 'https://i.imgur.com/5Hx4D9n.gif', 'https://i.imgur.com/j9Gvrrd.gif', 'https://i.imgur.com/jEmrZGS.gif', 'https://i.imgur.com/kn9awse.gif', 'https://i.imgur.com/AIJn1LF.mp4' 'https://i.imgur.com/uoaOqXO.gif', 'https://i.imgur.com/dJWlgnr.gif', 'https://i.imgur.com/G8Mbg1Z.gif', 'https://i.imgur.com/evjODur.gif', 'https://i.imgur.com/tumv7DY.gif', 'https://i.imgur.com/JZLaOA2.gifv']
+        choices = ['http://i.imgur.com/0D0Mijk.gif', 'https://i.imgur.com/4VePCc4.gif', 'http://i.imgur.com/3wv088f.gif', 'https://i.imgur.com/dG73Bmb.gif', '', 'https://i.imgur.com/KVLXsPN.gif', 'https://i.imgur.com/5Hx4D9n.gif', 'https://i.imgur.com/j9Gvrrd.gif', 'https://i.imgur.com/jEmrZGS.gif', 'https://i.imgur.com/kn9awse.gif', 'https://i.imgur.com/LLM3iPA.gif' 'https://i.imgur.com/uoaOqXO.gif', 'https://i.imgur.com/dJWlgnr.gif', 'https://i.imgur.com/G8Mbg1Z.gif', 'https://i.imgur.com/evjODur.gif', 'https://i.imgur.com/tumv7DY.gif', 'https://i.imgur.com/4fGLidc.gif']
         
         image = random.choice(choices)
         
@@ -72,7 +72,7 @@ class Gray:
         lick = "**{0} licked {1}!**"
         lickself="{} licked himself :thinking: "
         
-        choices = ['https://i.imgur.com/zIDBM1i.gif', 'https://i.imgur.com/44jdPuq.mp4', 'https://i.imgur.com/CxMooY1.gif', 'https://i.imgur.com/VRstYFu.gif', 'https://i.imgur.com/GP6brBA.gif', 'https://i.imgur.com/sPvTcLf.mp4', 'https://i.imgur.com/WTgUmla.mp4', 'https://i.imgur.com/i25AW7V.gif', 'https://i.imgur.com/ftuM6Ep.gif', 'https://i.imgur.com/LR8xv7v.gif', 'http://i.imgur.com/jMSv255.gif', 'https://i.imgur.com/wJ5iDyB.gif']
+        choices = ['https://i.imgur.com/zIDBM1i.gif', 'https://i.imgur.com/sPvTcLf.gif', 'https://i.imgur.com/CxMooY1.gif', 'https://i.imgur.com/VRstYFu.gif', 'https://i.imgur.com/GP6brBA.gif', 'https://i.imgur.com/WTgUmla.gif', 'https://i.imgur.com/44jdPuq.gif', 'https://i.imgur.com/i25AW7V.gif', 'https://i.imgur.com/ftuM6Ep.gif', 'https://i.imgur.com/LR8xv7v.gif', 'http://i.imgur.com/jMSv255.gif', 'https://i.imgur.com/wJ5iDyB.gif']
         
         image = random.choice(choices)
         
@@ -94,7 +94,7 @@ class Gray:
         pat = "**{} got patted by {}!**"
         patself="{} patted himelf :hugging: "
         
-        choices = ['http://i.imgur.com/10VrpFZ.gif', 'http://i.imgur.com/x0u35IU.gif', 'http://i.imgur.com/0gTbTNR.gif', 'http://i.imgur.com/hlLCiAt.gif', 'http://i.imgur.com/sAANBDj.gif', 'https://i.imgur.com/wtxwpm1.mp4', 'https://i.imgur.com/rQdg0Xr.gif', 'https://i.imgur.com/3eR7weH.gif', 'https://i.imgur.com/cK8Ro3x.gif', 'https://i.imgur.com/g1H0dHv.gif', 'https://i.imgur.com/qtHlt3n.gif', 'https://i.imgur.com/K3fad03.gif', 'https://i.imgur.com/bzzodCZ.mp4', 'https://i.imgur.com/usxXmP0.gif', 'https://i.imgur.com/mLyG5LV.gif', 'https://i.imgur.com/sIOXrRK.gif']
+        choices = ['http://i.imgur.com/10VrpFZ.gif', 'http://i.imgur.com/x0u35IU.gif', 'http://i.imgur.com/0gTbTNR.gif', 'http://i.imgur.com/hlLCiAt.gif', 'http://i.imgur.com/sAANBDj.gif', 'https://i.imgur.com/wtxwpm1.gif', 'https://i.imgur.com/rQdg0Xr.gif', 'https://i.imgur.com/3eR7weH.gif', 'https://i.imgur.com/cK8Ro3x.gif', 'https://i.imgur.com/g1H0dHv.gif', 'https://i.imgur.com/qtHlt3n.gif', 'https://i.imgur.com/K3fad03.gif', 'https://i.imgur.com/bzzodCZ.gif', 'https://i.imgur.com/usxXmP0.gif', 'https://i.imgur.com/mLyG5LV.gif', 'https://i.imgur.com/sIOXrRK.gif']
         
         image = random.choice(choices)
         
@@ -116,7 +116,7 @@ class Gray:
         slap = "**{} got slapped by {}**"
         slapself = "**{} slapped himelf**"
         
-        choices = ['https://i.imgur.com/EO8udG1.gif', 'https://i.imgur.com/lMmn1wy.gif', 'https://i.imgur.com/TuSUTg5.gif', 'https://i.imgur.com/9Ql97mO.gif', 'https://i.imgur.com/Qkv0q8n.gif', 'https://i.imgur.com/VBGqeIU.gif', 'https://i.imgur.com/uPZwGFQ.gif', 'https://i.imgur.com/Su0X9iF.gif', 'https://i.imgur.com/eNiOIMB.gif',  'https://i.imgur.com/gsAGyoI.gif', 'https://i.imgur.com/sF1BQg2.gif', 'https://i.imgur.com/zTiJjev.mp4']
+        choices = ['https://i.imgur.com/EO8udG1.gif', 'https://i.imgur.com/lMmn1wy.gif', 'https://i.imgur.com/TuSUTg5.gif', 'https://i.imgur.com/9Ql97mO.gif', 'https://i.imgur.com/Qkv0q8n.gif', 'https://i.imgur.com/VBGqeIU.gif', 'https://i.imgur.com/uPZwGFQ.gif', 'https://i.imgur.com/Su0X9iF.gif', 'https://i.imgur.com/eNiOIMB.gif',  'https://i.imgur.com/gsAGyoI.gif', 'https://i.imgur.com/sF1BQg2.gif', 'https://i.imgur.com/zTiJjev.gif']
         
         image = random.choice(choices)
         
